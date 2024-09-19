d2f2714ff7c72751bda45047cca0e17b9ef445f3
Sinash Shajahan <shajahan.sinash@gmail.com>
2024-09-18 23:17:05 -0400

    feat: add the required api routes, its controllers and services

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,30 @@
-import express, { Request, Response } from 'express';
+// Importing the Express framework
+import express from 'express';
+// Importing the global error handler middleware
+import { errorHandler } from './middleware/errorMiddleware';
+// Importing the routes for CPU load alerts, history, and current load
+import cpuLoadAlertsRoute from './routes/cpuLoadAlertsRoute';
+import cpuLoadHistoryRoute from './routes/cpuLoadHistoryRoute';
+import cpuLoadRoute from './routes/cpuLoadRoute';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
+// Create an instance of the Express application
 const app = express();
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-// Middleware to parse JSON
+// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+// Register routes for handling API requests related to CPU load, history, and alerts
+app.use('/api', cpuLoadRoute); // Route for current CPU load
+app.use('/api', cpuLoadHistoryRoute); // Route for CPU load history
+app.use('/api', cpuLoadAlertsRoute); // Route for CPU load alerts
+
+// Catch-all route for undefined routes, returning a 404 error
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found. Please check the URL.' });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Global error handler for handling errors throughout the application
+app.use(errorHandler);
+
+// Export the app instance to be used in other parts of the application (e.g., server setup)
+export default app;
